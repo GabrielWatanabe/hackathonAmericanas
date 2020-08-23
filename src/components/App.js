7763563ff93e67bb79b6e278c1bcf7d678912a25
Gabriel Yujii Ribeiro Watanabe <gabriel.watanabe@agrotools.local>
2020-08-23 10:08:53 -0300

    Foi adicionada a primeira tela com as ações dos botões e esta sendo desenvolvida a segunda tela

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,45 @@
 import React, { Component } from 'react';
-import header from '../assets/img/headerAmericanas.PNG'
+import header from '../assets/img/header.PNG'
+import headerAmericanas from '../assets/img/headerAmericanas.PNG'
 import Surpreenda from './meSurpreenda'
+import './styles.css'
 
 export default class App extends Component {
+
+    
+    constructor() {
+        super();
+        
+        this.state = {
+          clicked: false
+        };
+        this.showSurpreenda = this.showSurpreenda.bind(this);
+      }
+
+    showSurpreenda() {
+        console.log(this.state.clicked)
+        this.setState({
+            clicked: true
+        });
+    }
+    
+
     render() {
         return(
             <div>
-                <img src={header}></img>
-                <Surpreenda></Surpreenda>
+                {this.state.clicked ? <Surpreenda /> : 
+                <div>
+                <img className="img" src={header}></img>
+                <img className="img" src={headerAmericanas}></img>
+                <div className="divButtonClickOne">
+                    <button onClick={this.showSurpreenda} className="buttonOne"></button>
+                </div>
+                <div className="divButtonClickTwo">
+                    <button className="buttonTwo"></button>
+                </div>
+                </div>
+               }
             </div>
-
         )
     }
 }
